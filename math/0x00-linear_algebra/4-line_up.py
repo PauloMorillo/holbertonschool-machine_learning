fc25db1c25a5e84b826ae9ab7653d5b96e5c64c9
PauloMorillo <pauloan@hotmail.com>
2020-04-16 20:07:29 +0000

    [update] remove condition

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -4,12 +4,11 @@
 
 def add_arrays(arr1, arr2):
     """ This function returns the add between two arrays"""
-    if arr1 and arr2:
-        if len(arr1) == len(arr2):
-            a = 0
-            new_vector = []
-            while a < len(arr1):
-                new_vector = new_vector + [arr1[a] + arr2[a]]
-                a = a + 1
-            return new_vector
+    if len(arr1) == len(arr2):
+        a = 0
+        new_vector = []
+        while a < len(arr1):
+            new_vector = new_vector + [arr1[a] + arr2[a]]
+            a = a + 1
+        return new_vector
     return None
