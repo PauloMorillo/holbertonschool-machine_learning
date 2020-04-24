4bca41de1cf62eb8b8b48372fcc7d33ddaa9fe2c
PauloMorillo <pauloan@hotmail.com>
2020-04-24 18:19:07 +0000

    back to the conditionals

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -9,10 +9,10 @@ def poly_integral(poly, C=0):
         for exp in range(len(poly)):
             if type(poly[exp]) == int:
                 newesc = (int(poly[exp] * (exp + 1)) / int(exp + 1))
-                # if (newesc % (exp + 1)) == 0:
-                ans = ans + [(int(newesc) / int(exp + 1)).__round__(2)]
-                # else:
-                # new_list = new_list + [(int(newesc) / int(exp + 1))]
+                if (newesc % (exp + 1)) == 0:
+                    ans = ans + [(int(newesc) // int(exp + 1))]
+                else:
+                    ans = ans + [(int(newesc) / int(exp + 1)).__round__(2)]
             else:
                 return None
         return ans
