47624271a8df4ddc2d9d26be9e8cec8fca691c13
PauloMorillo <pauloan@hotmail.com>
2020-04-24 13:06:31 +0000

    remove condition

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -9,10 +9,10 @@ def poly_integral(poly, C=0):
         for exp in range(len(poly)):
             if type(poly[exp]) == int:
                 newesc = (int(poly[exp] * (exp + 1)) / int(exp + 1))
-                if (newesc % (exp + 1)) == 0:
-                    new_list = new_list + [(int(newesc) // int(exp + 1))]
-                else:
-                    new_list = new_list + [(int(newesc) / int(exp + 1))]
+                # if (newesc % (exp + 1)) == 0:
+                new_list = new_list + [(int(newesc) / int(exp + 1))]
+                # else:
+                # new_list = new_list + [(int(newesc) / int(exp + 1))]
             else:
                 return None
         return new_list
