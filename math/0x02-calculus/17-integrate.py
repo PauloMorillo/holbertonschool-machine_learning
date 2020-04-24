91dc29eafdfbbd5b9e0e725e80226a0d399f8eb9
PauloMorillo <pauloan@hotmail.com>
2020-04-24 18:11:47 +0000

    add round

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -5,15 +5,15 @@
 def poly_integral(poly, C=0):
     """ Funtion solve polynomial integrals """
     if poly and type(poly) == list and len(poly) > 0 and type(C) == int:
-        new_list = [0]
+        ans = [0]
         for exp in range(len(poly)):
             if type(poly[exp]) == int:
                 newesc = (int(poly[exp] * (exp + 1)) / int(exp + 1))
                 # if (newesc % (exp + 1)) == 0:
-                new_list = new_list + [(int(newesc) / int(exp + 1))]
+                ans = ans + [(int(newesc) / int(exp + 1)).__round__(2)]
                 # else:
                 # new_list = new_list + [(int(newesc) / int(exp + 1))]
             else:
                 return None
-        return new_list
+        return ans
     return None
