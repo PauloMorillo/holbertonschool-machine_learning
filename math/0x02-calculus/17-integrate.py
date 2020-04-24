e276050abaa6a812de51bb4cece1b3d388fb77d5
PauloMorillo <pauloan@hotmail.com>
2020-04-24 18:34:40 +0000

    add complement

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -4,16 +4,17 @@
 
 def poly_integral(poly, C=0):
     """ Funtion solve polynomial integrals """
-    if poly and type(poly) == list and len(poly) > 0 and type(C) == int:
-        ans = [0]
-        for exp in range(len(poly)):
-            if type(poly[exp]) == int:
-                newesc = (int(poly[exp] * (exp + 1)) / int(exp + 1))
-                if (newesc % (exp + 1)) == 0:
-                    ans = ans + [(int(newesc) // int(exp + 1))]
+    if poly and type(poly) == list:
+        if len(poly) > 0 and type(C) in [int, float]:
+            ans = [C]
+            for exp in range(len(poly)):
+                if type(poly[exp]) == int:
+                    newesc = (int(poly[exp] * (exp + 1)) / int(exp + 1))
+                    if (newesc % (exp + 1)) == 0:
+                        ans = ans + [(int(newesc) // int(exp + 1))]
+                    else:
+                        ans = ans + [(int(newesc) / int(exp + 1)).__round__(2)]
                 else:
-                    ans = ans + [(int(newesc) / int(exp + 1)).__round__(2)]
-            else:
-                return None
-        return ans
+                    return None
+            return ans
     return None
