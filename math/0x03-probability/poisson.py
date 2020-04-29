5ea2597d0039ee23f571c98dea431eb3ccf51438
PauloMorillo <pauloan@hotmail.com>
2020-04-29 14:33:23 +0000

    [fix] correct way to rise an error

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -11,12 +11,12 @@ class Poisson():
         self.data = []
         if data is not None:
             if type(data) != list:
-                raise(TypeError, "data must be a list")
+                raise TypeError("data must be a list")
             if len(data) < 2:
-                raise(ValueError, "data must contain multiple values")
+                raise ValueError('data must contain multiple values')
             self.lambtha = sum(data)/len(data)
         else:
             if lambtha >= 1:
                 self.lambtha = float(lambtha)
             else:
-                raise(ValueError, "lambtha must be a positive value")
+                raise ValueError('lambtha must be a positive value')
