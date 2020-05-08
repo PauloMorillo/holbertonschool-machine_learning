73c76d8402b29bf6f9ed9a5e27fa9aa711786aa3
PauloMorillo <pauloan@hotmail.com>
2020-05-07 19:03:16 -0500

    [Update] add condition in case

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -14,7 +14,7 @@ class DeepNeuralNetwork():
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if type(layers) is not list:
+        if type(layers) is not list or len(layers) < 1:
             raise TypeError("layers must be a list of positive integers")
         weights = {}
         lay = 0
