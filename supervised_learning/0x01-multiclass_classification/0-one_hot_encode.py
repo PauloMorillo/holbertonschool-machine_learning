f08bf4b7b3b1bddd6be87de1381755f1abba52aa
PauloMorillo <pauloan@hotmail.com>
2020-05-12 12:08:12 -0500

    [Update] only condition

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -8,9 +8,9 @@ def one_hot_encode(Y, classes):
     """This method encode a numerical label vector into
     a one-hot matrix
     """
-    if type(classes) is not int or classes <= np.amax(Y):
-        return None
     if Y is None:
+        return None
+    if type(classes) is not int or classes <= np.amax(Y):
         return None
     encoding_y = np.zeros((classes, len(Y)))
     for i in range(len(Y)):
