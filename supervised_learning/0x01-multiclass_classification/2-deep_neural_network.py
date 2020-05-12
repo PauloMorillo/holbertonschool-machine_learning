22bb873b6c794e056ec1676c014c20ac49ff8c97
PauloMorillo <pauloan@hotmail.com>
2020-05-12 16:16:03 -0500

    [Update] only condition

--- supervised_learning/0x01-multiclass_classification/2-deep_neural_network.py
+++ supervised_learning/0x01-multiclass_classification/2-deep_neural_network.py
@@ -106,57 +106,59 @@ class DeepNeuralNetwork():
 
     def train(self, X, Y, iterations=5000, alpha=0.05,
               verbose=True, graph=True, step=100):
-        """ This method trains the DNN """
+        """
+        Complete Train of the deep neural network
+        """
         if type(iterations) is not int:
             raise TypeError("iterations must be an integer")
-        if iterations <= 0:
+        if iterations < 1:
             raise ValueError("iterations must be a positive integer")
         if type(alpha) is not float:
             raise TypeError("alpha must be a float")
         if alpha <= 0:
             raise ValueError("alpha must be positive")
 
-        allcost = []
-        xcost = []
+        i = 0
+        epochs, costs = [], []
 
-        for i in range(iterations + 1):
-            A, c = self.evaluate(X, Y)
-            if verbose:
-                if i % step == 0 or i == iterations:
-                    print("Cost after {} iterations: {}".format(i, c))
-            if graph:
-                if i % step == 0 or i == iterations:
-                    allcost = allcost + [c]
-                    xcost = xcost + [i]
+        while (i < iterations + 1):
             A, cache = self.forward_prop(X)
-            self.gradient_descent(Y, cache, alpha)
+            if i % step == 0 or i == iterations:
+                cost = self.cost(Y, A)
+                epochs.append(i)
+                costs.append(cost)
+                if verbose is True:
+                    w = "Cost after {} iterations: {}".format(i, cost)
+                    print(w)
+            if i < iterations:
+                self.gradient_descent(Y, cache, alpha)
+            i += 1
 
-        if graph:
+        if graph is True:
+            plt.plot(epochs, costs)
             plt.xlabel('iteration')
             plt.ylabel('cost')
-            plt.title('Training Cost')
-            plt.plot(xcost, allcost, 'b')
+            plt.suptitle("Training Cost")
             plt.show()
-        return (self.evaluate(X, Y))
+
+        return self.evaluate(X, Y)
+
 
     def save(self, filename):
         """
-        save an object in a file with pickle module
-        it uses serialization
+         This method saves the instance object to a file in pickle
         """
-        if filename[-4:] != ".pkl":
-            filename += ".pkl"
-        with open(filename, 'wb') as fd:
-            pickle.dump(self, fd)
+        if '.pkl' != filename[-4:]:
+            filename = filename + '.pkl'
+        with open(filename, 'wb') as f:
+            pickle.dump(self, f)
 
     def load(filename):
         """
-        load the object stored in the pickle file
-        i uses deserialization
+        This method loads a pickled DNN object
         """
         if os.path.exists(filename) is True:
-            with open(filename, 'rb') as fd:
-                object = pickle.load(fd)
-            return object
-        else:
-            return None
+            with open(filename, 'rb') as f:
+                dnn = pickle.load(f)
+            return dnn
+        return None
