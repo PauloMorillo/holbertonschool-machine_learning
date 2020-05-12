9f3747948abc637850b273b6c6793f4ba9c9493a
PauloMorillo <pauloan@hotmail.com>
2020-05-12 11:01:58 -0500

    [Update] only condition

--- supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -9,6 +9,15 @@ def one_hot_decode(one_hot):
     This method decode a one-hot matrix into a numerical label
     """
     try:
-        return np.argmax(one_hot, axis=0)
+        decoding = np.zeros(one_hot.shape[0])
+        rowspos = 0
+        for row in one_hot:
+            colspos = 0
+            for col in row:
+                if col == 1:
+                    decoding[colspos] = rowspos
+                colspos = colspos + 1
+            rowspos = rowspos + 1
     except E:
         return None
+    return decoding.astype(int)
