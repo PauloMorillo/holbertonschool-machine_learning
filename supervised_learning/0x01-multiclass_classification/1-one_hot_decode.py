f4521cc2019ce1a032e98d510ccec53f19660044
PauloMorillo <pauloan@hotmail.com>
2020-05-12 10:03:54 -0500

    [Update] remove loops return only argmax

--- supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -9,15 +9,7 @@ def one_hot_decode(one_hot):
     This method decode a one-hot matrix into a numerical label
     """
     try:
-        decoding = np.zeros(one_hot.shape[0])
-        rowspos = 0
-        for row in one_hot:
-            colspos = 0
-            for col in row:
-                if col == 1:
-                    decoding[colspos] = rowspos
-                colspos = colspos + 1
-            rowspos = rowspos + 1
+        pos = np.argmax(one_hot, axis=0)
     except:
         return None
-    return decoding.astype(int)
+    return pos
