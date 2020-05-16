1f4a1e0217602eb14e621eaa07fea33c591e6801
PauloMorillo <pauloan@hotmail.com>
2020-05-16 17:42:10 +0000

    [update] add 0 for floats

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -5,5 +5,5 @@ import tensorflow as tf
 
 def calculate_accuracy(y, y_pred):
     """ This method calculates accuracy for the NN """
-    accu = tf.reduce_mean(tf.cast(tf.equal(y, y_pred), tf.float32))
+    accu = tf.reduce_mean(tf.cast(tf.equal(y, y_pred), tf.float32), 0)
     return accu
