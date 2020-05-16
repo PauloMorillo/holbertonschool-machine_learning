f87e224a6d5f761c16e2c04d3f5cbaf8d26112c3
PauloMorillo <pauloan@hotmail.com>
2020-05-16 18:15:17 +0000

    [update] using argmax and equal and cast

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -5,5 +5,6 @@ import tensorflow as tf
 
 def calculate_accuracy(y, y_pred):
     """ This method calculates accuracy for the NN """
-    accu = tf.reduce_mean((y_pred, y))
+    correc = tf.equal(tf.argmax(y, 1), tf.argmax(y_pred, 1))
+    accu = tf.reduce_mean(tf.cast(correc, tf.float32))
     return accu
