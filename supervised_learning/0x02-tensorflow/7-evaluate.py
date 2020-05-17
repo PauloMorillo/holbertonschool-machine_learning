8df3e912b051f21e730ae567cd5ca5678d4c62ed
root <root@compute-instance-ide-hol-paulo-morillo-36-29.c.maximal-grin-211714.internal>
2020-05-17 07:18:00 +0000

    [update] indexing tensors

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -8,14 +8,15 @@ def evaluate(X, Y, save_path):
     sess = tf.Session()
     saver = tf.train.import_meta_graph(save_path + '.meta')
     saver.restore(sess, save_path)
-    x = tf.get_collection('x')
-    y = tf.get_collection('y')
-    y_pred = tf.get_collection('y_pred')
-    loss = tf.get_collection('loss')
-    accuracy = tf.get_collection('accuracy')
+    x = tf.get_collection('x')[0]
+    y = tf.get_collection('y')[0]
+    y_pred = tf.get_collection('y_pred')[0]
+    print(y_pred)
+    accuracy = tf.get_collection('accuracy')[0]
+    loss = tf.get_collection('loss')[0]
     train_op = tf.get_collection('train_op')
-    sess.run((train_op), feed_dict={x: X, y: Y})
-    y_predr, accuar, lossr = sess.run((y_pred, accua, loss),
-                                      feed_dict={x: X, y: Y})
+    y_predr = sess.run((y_pred), feed_dict={x: X, y: Y})
+    accuar = sess.run((accuracy), feed_dict={x: X, y: Y})
+    lossr = sess.run((loss), feed_dict={x: X, y: Y})
     sess.close()
     return y_predr, accuar, lossr
