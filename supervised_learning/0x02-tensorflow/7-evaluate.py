b4d4f382ffd31923f0693ce3a0f8d2063280dcac
PauloMorillo <pauloan@hotmail.com>
2020-05-17 16:14:29 +0000

    [update] indexing tensors

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -5,16 +5,16 @@ import tensorflow as tf
 
 def evaluate(X, Y, save_path):
     """ This method restore a model and evaluate """
+    init = tf.global_variables_initializer()
     sess = tf.Session()
+    sess.run(init)
     saver = tf.train.import_meta_graph(save_path + '.meta')
     saver.restore(sess, save_path)
     x = tf.get_collection('x')[0]
     y = tf.get_collection('y')[0]
     y_pred = tf.get_collection('y_pred')[0]
-    print(y_pred)
-    accuracy = tf.get_collection('accuracy')[0]
     loss = tf.get_collection('loss')[0]
-    train_op = tf.get_collection('train_op')
+    accuracy = tf.get_collection('accuracy')[0]
     y_predr = sess.run((y_pred), feed_dict={x: X, y: Y})
     accuar = sess.run((accuracy), feed_dict={x: X, y: Y})
     lossr = sess.run((loss), feed_dict={x: X, y: Y})
