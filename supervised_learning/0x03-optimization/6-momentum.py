ab2379e07318d74a106bfeefa6de4ca7c0466e58
PauloMorillo <pauloan@hotmail.com>
2020-05-21 00:07:49 -0500

    [Update] Added calculates with tensorflow momentum gradient descent

---
+++ supervised_learning/0x03-optimization/6-momentum.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" This module has the create_momentum_op method"""
+import tensorflow as tf
+
+
+def create_momentum_op(loss, alpha, beta1):
+    """
+    creates the training operation for a neural network
+    """
+    operation = tf.train.MomentumOptimizer(alpha, beta1)
+    train = operation.minimize(loss)
+    return loss
