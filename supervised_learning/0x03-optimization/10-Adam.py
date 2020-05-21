52492b2a1331475e7c16f4a631c2df4af62bbb7d
PauloMorillo <pauloan@hotmail.com>
2020-05-21 11:13:46 -0500

    [Update] Added Adam gradient descent

--- supervised_learning/0x03-optimization/10-Adam.py
+++ supervised_learning/0x03-optimization/10-Adam.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-""" This module has the create_RMSProp_op method"""
+""" This module has the create_Adam_op method"""
 import tensorflow as tf
 
 
-def create_RMSProp_op(loss, alpha, beta2, epsilon):
+def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
     """
     creates the training operation for a neural network
     """
-    operation = tf.train.RMSPropOptimizer(alpha, beta2, epsilon)
+    operation = tf.train.AdamOptimizer(alpha, beta1, beta2, epsilon)
     train = operation.minimize(loss)
     return train
---
+++ supervised_learning/0x03-optimization/9-Adam.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" This module has the update_variables_RMSProp method"""
+import numpy as np
+
+
+def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
+    """
+    This method updates a variable in place
+    using the Adam optimization algorithm
+    """
+    v = (beta1 * v) + ((1 - beta1) * grad)
+    vcorrec = v / (1 - (beta1 ** t))
+    s = (beta2 * s) + ((1 - beta2) * grad ** 2)
+    scorrec = np.divide(s, (1 - beta2 ** t))
+    var = var - (alpha * (vcorrec / (np.sqrt(scorrec) + epsilon)))
+    return var, vcorrec, scorrec
