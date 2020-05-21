b34ec54b54977f69b7535c84719fa3706dfe9375
PauloMorillo <pauloan@hotmail.com>
2020-05-21 13:09:58 -0500

    [fix] returning prevs without correction

--- supervised_learning/0x03-optimization/9-Adam.py
+++ supervised_learning/0x03-optimization/9-Adam.py
@@ -11,6 +11,6 @@ def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
     v = (beta1 * v) + ((1 - beta1) * grad)
     vcorrec = v / (1 - (beta1 ** t))
     s = (beta2 * s) + ((1 - beta2) * grad ** 2)
-    scorrec = np.divide(s, (1 - beta2 ** t))
+    scorrec = s / (1 - beta2 ** t)
     var = var - (alpha * (vcorrec / (np.sqrt(scorrec) + epsilon)))
-    return var, vcorrec, scorrec
+    return var, v, s
