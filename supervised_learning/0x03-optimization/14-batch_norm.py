b8ced17ff5e2991231ede688da4a47d6e0d67e49
root <root@compute-instance-ide-hol-paulo-morillo-36-29.c.maximal-grin-211714.internal>
2020-05-22 02:12:45 +0000

    [fix] remove numpy

--- supervised_learning/0x03-optimization/14-batch_norm.py
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """ This module has the learning_rate_decay method"""
 import tensorflow as tf
-import numpy as np
 
 
 def create_batch_norm_layer(prev, n, activation):
@@ -13,7 +12,6 @@ def create_batch_norm_layer(prev, n, activation):
     kernel = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     y_pred = tf.layers.dense(prev, units=n,
                              kernel_initializer=kernel)
-
     mean, var = tf.nn.moments(y_pred, [0], keep_dims=True)
     gamma = tf.Variable(tf.ones([y_pred.get_shape()[-1]]))
     beta = tf.Variable(tf.zeros([y_pred.get_shape()[-1]]))
