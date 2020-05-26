70a9804a454f572e46e9a168e46c622a052dfd25
PauloMorillo <pauloan@hotmail.com>
2020-05-26 11:48:32 -0500

    [Create] added 6 task

--- supervised_learning/0x04-error_analysis/3-specificity.py
+++ supervised_learning/0x04-error_analysis/3-specificity.py
@@ -6,11 +6,12 @@ import numpy as np
 def specificity(confusion):
     """This method calculates the specificity
     for each class in a confusion matrix"""
-    allv = confusion.T.sum(axis=1)
+    allv = confusion.sum(axis=0)
     allh = confusion.sum(axis=1)
-    tp = confusion.diagonal()
-    true_negative = tp
-    fp = allv - tp
-    print(fp)
-    tn = tp - (allh + allv)
-    return tn / (tn + fp)
+    diagonal = confusion.diagonal()
+    n = allh.sum() - allh
+    fp = allv - diagonal
+    tn2 = n - fp
+    tn = diagonal.sum() - diagonal
+    # print(tn / (tn + fp))
+    return tn2 / n
