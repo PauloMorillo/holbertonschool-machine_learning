37d3f6c339869f98c42aad1cf43e704552616935
PauloMorillo <pauloan@hotmail.com>
2020-05-29 10:02:31 -0500

    [Create] added 7 task

---
+++ supervised_learning/0x05-regularization/7-early_stopping.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""This module has the method early_stopping"""
+
+
+def early_stopping(cost, opt_cost, threshold, patience, count):
+    """This method determines if you should stop gradient descent early"""
+    delta = abs(cost - opt_cost)
+    verify = False
+    if delta > threshold:
+        count = 0
+    else:
+        count = count + 1
+        if patience == count:
+            verify = True
+    return verify, count
