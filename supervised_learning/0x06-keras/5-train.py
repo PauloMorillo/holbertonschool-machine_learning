458ba5bba5a2aae6e6b55b79bfe170139c2b2157
PauloMorillo <pauloan@hotmail.com>
2020-06-02 08:15:42 -0500

    [Update] added conditional for validation data

--- supervised_learning/0x06-keras/5-train.py
+++ supervised_learning/0x06-keras/5-train.py
@@ -10,7 +10,7 @@ import tensorflow.keras as K
 def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, verbose=True, shuffle=False):
     """ This method train a model using mini-batch gradient descent"""
-    if validation_data is not tuple:
+    if type(validation_data) is not tuple:
         validation_data = None
     network.fit(data, labels, epochs=epochs,
                 batch_size=batch_size,
