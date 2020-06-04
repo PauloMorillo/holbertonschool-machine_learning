6b7267f15320caea4865661df069f9fc941b74ab
PauloMorillo <pauloan@hotmail.com>
2020-06-04 17:39:54 -0500

    [update] added formulas to calculate the matrix output

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -9,29 +9,30 @@ def convolve_grayscale_same(images, kernel):
     """This method performs a same
     convolution on grayscale images
     """
-    rows_im = images.shape[1]
-    cols_im = images.shape[2]
-    rows_k = kernel.shape[0]
-    cols_k = kernel.shape[1]
-    pw = max((cols_im - cols_k) * 1 + (cols_k - cols_im), 0)
-    ph = max((rows_im - rows_k) * 1 + (rows_k - rows_im), 0)
-    pw_r = pw // 2
-    ph_t = ph // 2
-    pw_l = pw - pw_r
-    ph_b = ph - ph_t
-    images = np.pad(images, ((0, 0), (ph_t, ph_b), (pw_l, pw_r)),
+    h = images.shape[1]
+    w = images.shape[2]
+    kh = kernel.shape[0]
+    kw = kernel.shape[1]
+    output_h = (w - kw) + 1
+    # pw = max((output_w - cols_k) * 1 + (cols_k - cols_im), 0)
+    # ph = max((rows_im - rows_k) * 1 + (rows_k - rows_im), 0)
+    if h % 2 == 0:
+        ph = h // 2
+    else:
+        ph = (h - 1) // 2
+    if w % 2 == 0:
+        pw = w // 2
+    else:
+        pw = (w - 1) // 2
+    images = np.pad(images, ((0, 0), (ph, ph), (pw, pw)),
                     'constant', constant_values=0)
-    rows_im = images.shape[1]
-    cols_im = images.shape[2]
-    new_rows = (rows_im - rows_k) + 1
-    new_cols = (cols_im - cols_k) + 1
-    # print(new_cols, new_rows)
-    new = np.zeros((images.shape[0], new_rows, new_cols))
+
+    new = np.zeros((images.shape[0], h, w))
     # print(new.shape)
     # print(new)
     for i in range(new.shape[1]):
         for j in range(new.shape[2]):
-            ans = images[:, i:rows_k + i, j:cols_k + j] * kernel
+            ans = images[:, i:kh + i, j:kw + j] * kernel
             # print(ans.shape)
             # print(ans.T.shape)
             # print(np.sum(ans, axis=2).shape)
