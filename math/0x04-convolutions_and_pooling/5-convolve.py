0976eee8ad2c7fb8d6cc4fdf0aa583a7fa1915e8
PauloMorillo <pauloan@hotmail.com>
2020-06-05 14:12:32 -0500

    [update] added correct way to use padding in theory

--- math/0x04-convolutions_and_pooling/5-convolve.py
+++ math/0x04-convolutions_and_pooling/5-convolve.py
@@ -9,41 +9,62 @@ def convolve(images, kernels, padding='same', stride=(1, 1)):
     """
     This method performs a convolution on images with channels
     """
+    h = images.shape[1]
+    w = images.shape[2]
+    kh = kernels.shape[0]
+    kw = kernels.shape[1]
+    sh = stride[0]
+    sw = stride[1]
+    output_h = int(np.ceil((h - kh + 1) / sh))
+    output_w = int(np.ceil((w - kw + 1) / sw))
+
     if type(padding) is tuple:
+        output_w = ((w - kw + (2 * padding[1])) // sw) + 1
+        output_h = ((h - kh + (2 * padding[0])) // sh) + 1
         images = np.pad(images, ((0, 0), (padding[0], padding[0]),
-                                 (padding[1], padding[1])),
+                                 (padding[1], padding[1]), (0, 0)),
                         'constant', constant_values=0)
     else:
         if padding == "same":
-            images = np.pad(images, ((0, 0), (1, 1), (1, 1), (0, 0)),
+            output_h = int(np.ceil(h / sh))
+            output_w = int(np.ceil(w / sw))
+            output_w = w
+            output_h = h
+            if kh % 2 == 0:
+                pt = kh // 2
+                pb = kh // 2
+            else:
+                ph = max((output_h - 1) * stride[0] + kh - h, 0)
+                pt = ph // 2
+                pb = ph - pt
+            if kw % 2 == 0:
+                pt = kh // 2
+                pb = kh // 2
+            else:
+                pw = max((output_w - 1) * stride[1] + kw - w, 0)
+                pl = pw // 2
+                pr = pw - pl
+            ph = int(((h - 1) * sh + kh - h) / 2) + 1
+            pw = int(((w - 1) * sw + kw - w) / 2) + 1
+            # print(ph, pw)
+            images = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
                             'constant', constant_values=0)
 
-    rows_im = images.shape[1]
-    cols_im = images.shape[2]
-    # print(kernels)
-    # print(kernels[:,:,:,0])
-    rows_k = kernels.shape[1]
-    cols_k = kernels.shape[2]
-    new_rows = (rows_im - rows_k + 1)
-    new_cols = (cols_im - cols_k + 1)
-    dimension = (images.shape[3])
-    # print(new_cols, new_rows)
-    new = np.ones((images.shape[0], new_rows // stride[0],
-                   new_cols // stride[1], images.shape[3]))
-    # print(new.shape)
+    new = np.ones((images.shape[0], output_h,
+                   output_w, kernels.shape[0]))  # print(new.shape)
     # print(new)
     new_r = 0
 
-    for i in range(0, new_rows, stride[0]):
+    for i in range(0, output_h * sh, sh):
         new_c = 0
-        for j in range(0, new_cols, stride[1]):
+        for j in range(0, output_w * sw, sw):
             for k in range(len(kernels)):
-                ans = images[:, i:rows_k + i, j:cols_k + j, :] * \
+                ans = images[:, i:kh + i, j:kw + j, :] * \
                       kernels[:, :, :, k]
                 # print(ans.shape)
                 # print(ans.T.shape)
                 # print(np.sum(ans, axis=2).shape)
-                mat = np.sum(np.sum(ans, axis=1), axis=2)
+                mat = np.sum(ans, axis=(1, 3))
                 # print(mat.shape)
                 new[:, new_r, new_c, k] = np.sum(mat, axis=1)
             new_c = new_c + 1
