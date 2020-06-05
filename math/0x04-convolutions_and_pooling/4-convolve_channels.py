5e4ba7597a5abad77a1634326f144a41562ad959
PauloMorillo <pauloan@hotmail.com>
2020-06-05 13:15:28 -0500

    [update] added shape for kernels in same

--- math/0x04-convolutions_and_pooling/4-convolve_channels.py
+++ math/0x04-convolutions_and_pooling/4-convolve_channels.py
@@ -45,8 +45,8 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
                 pw = max((output_w - 1) * stride[1] + kw - w, 0)
                 pl = pw // 2
                 pr = pw - pl
-            ph = int(np.ceil(((h - 1) * sh + kh - h) / 2) + 1)
-            pw = int(np.ceil(((w - 1) * sw + kw - w) / 2) + 1)
+            ph = int(((h - 1) * sh + kh - h) / 2) + 1
+            pw = int(((w - 1) * sw + kw - w) / 2) + 1
             # print(ph, pw)
             images = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
                             'constant', constant_values=0)
