f102705c3a7527a997fc924a8d45e875e4b5641e
PauloMorillo <pauloan@hotmail.com>
2020-06-17 15:13:09 -0500

    [Fix] changed relu function with relu string

--- supervised_learning/0x08-deep_cnns/5-dense_block.py
+++ supervised_learning/0x08-deep_cnns/5-dense_block.py
@@ -7,11 +7,17 @@ import tensorflow.keras as K
 
 
 def dense_block(X, nb_filters, growth_rate, layers):
+    """
+    X is the output from the previous layer
+    nb_filters is an integer representing the number of filters in X
+    growth_rate is the growth rate for the dense block
+    layers is the number of layers in the dense block
+    """
     kernel = "he_normal"
     for i in range(layers):
         norm1 = K.layers.BatchNormalization()(X)
         act1 = K.layers.Activation("relu")(norm1)
-        con1 = K.layers.Conv2D((2 * growth_rate) + nb_filters, (1, 1),
+        con1 = K.layers.Conv2D((4 * growth_rate), (1, 1),
                                padding="same",
                                kernel_initializer=kernel)(act1)
         norm2 = K.layers.BatchNormalization()(con1)
