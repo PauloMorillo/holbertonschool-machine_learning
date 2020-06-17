94fa64bbceabc537c2ddad855a0721a433f94e7b
PauloMorillo <pauloan@hotmail.com>
2020-06-17 15:48:34 -0500

    [update] added script for task 6

---
+++ supervised_learning/0x08-deep_cnns/6-transition_layer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+This script has the method
+transition_layer(X, nb_filters, compression)
+"""
+import tensorflow.keras as K
+
+
+def transition_layer(X, nb_filters, compression):
+    """
+    X is the output from the previous layer
+    nb_filters is an integer representing the number of filters in X
+    compression is the compression factor for the transition layer
+    """
+    kernel = "he_normal"
+    norm1 = K.layers.BatchNormalization()(X)
+    act1 = K.layers.Activation("relu")(norm1)
+    filt = int(nb_filters * compression)
+    print(filt)
+    con1 = K.layers.Conv2D(filt, (1, 1),
+                           padding="same",
+                           kernel_initializer=kernel)(act1)
+    Y = K.layers.AveragePooling2D((2, 2))(con1)
+    return Y, Y.shape[-1]
