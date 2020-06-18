93d1089cb91df397baebfa4bfe1f0f22b38e906a
PauloMorillo <pauloan@hotmail.com>
2020-06-18 12:19:26 -0500

    [update] Added import functions

--- supervised_learning/0x08-deep_cnns/4-resnet50.py
+++ supervised_learning/0x08-deep_cnns/4-resnet50.py
@@ -4,6 +4,8 @@ This script has the method
 resnet50()
 """
 import tensorflow.keras as K
+identity_block = __import__('2-identity_block').identity_block
+projection_block = __import__('3-projection_block').projection_block
 
 
 def resnet50():
