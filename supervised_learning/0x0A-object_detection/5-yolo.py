d596b4c460819057de3768f0989183320880355c
PauloMorillo <pauloan@hotmail.com>
2020-07-02 19:29:30 -0500

    [update] remove iunnecessary print

--- supervised_learning/0x0A-object_detection/5-yolo.py
+++ supervised_learning/0x0A-object_detection/5-yolo.py
@@ -45,10 +45,10 @@ class Yolo():
 
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         """ Everything begins here """
-        # self.model = K.models.load_model(model_path)
-        # with open(classes_path, 'r') as f:
-        #    data = f.read()
-        # self.class_names = data.split()
+        self.model = K.models.load_model(model_path)
+        with open(classes_path, 'r') as f:
+            data = f.read()
+        self.class_names = data.split()
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
