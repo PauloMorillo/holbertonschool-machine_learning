d625a86fdb3275109aa55eb268208177af0d8954
PauloMorillo <pauloan@hotmail.com>
2020-07-23 15:24:33 -0500

    [change] directory in worng path

---
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+This module has the method determinant(matrix)
+"""
+
+
+def determinant(matrix):
+    """
+    This method calculates the determinant of a matrix
+    """
+    if type(matrix) is not list \
+            or len(matrix) < 1 or \
+            type(matrix[0]) is not list:
+        raise TypeError('matrix must be a list of lists')
+
+    rows_length = len(matrix)
+    for row in matrix:
+        if len(row) is not rows_length and len(row) is not 0:
+            raise ValueError('matrix must be a square matrix')
+
+    if rows_length is 1:
+        if len(matrix[0]) is 0:
+            return 1
+        return matrix[0][0]
+    elif rows_length is 2:
+        a = matrix[0][0]
+        b = matrix[0][1]
+        c = matrix[1][0]
+        d = matrix[1][1]
+        det = (a * d) - (b * c)
+        return det
+    else:
+        det = 0
+        for j0 in range(len(matrix[0])):
+            new_ma = []
+            for i in range(1, len(matrix)):
+                row = []
+                for j in range(len(matrix[i])):
+                    if j is not j0:
+                        row.append(matrix[i][j])
+                new_ma.append(row)
+            if j0 % 2 is not 0:
+                factor = -1
+            else:
+                factor = 1
+            second_fact = determinant(new_ma)
+            det = det + (factor * (matrix[0][j0] * (second_fact)))
+        return det
---
+++ math/0x05-advanced_linear_algebra/README.md
@@ -0,0 +1,36 @@
+# Advanced Linear Algebra
+
+This directory has the exercises to learn a little bit of determinant, minor, inverse, eigenvalues, eigenvectors and definiteness
+
+## Before to code
+
+Topics to learn:
+
+What is a determinant? How would you calculate it?
+What is a minor, cofactor, adjugate? How would calculate them?
+What is an inverse? How would you calculate it?
+What are eigenvalues and eigenvectors? How would you calculate them?
+What is definiteness of a matrix? How would you determine a matrix’s definiteness?
+
+## Requirements
+Numpy and pycodestyle modules
+
+
+```bash
+pip install --user numpy==1.15
+pip install --user pycodestyle==2.5
+
+```
+
+##Running main files
+```bash
+./0-main.py
+
+```
+
+
+## Author
+[Paulo Morillo](https://www.linkedin.com/in/paulo-morillo-mu%C3%B1oz-191745143/)
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
