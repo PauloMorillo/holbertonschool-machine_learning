5d950098f7ac55b072806bbc0dfc013c36b09657
PauloMorillo <pauloan@hotmail.com>
2020-07-27 18:38:31 -0500

    [Create] Added directory and script 1

---
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""This module has the method correlation(C)"""
+
+import numpy as np
+
+
+def correlation(C):
+    """This function calculates correlation"""
+
+    if type(C) is not np.ndarray or np.ndim(C) is not 2:
+        raise TypeError('C must be a numpy.ndarray')
+    if C.shape[0] is not C.shape[1]:
+        raise ValueError('C must be a 2D square matrix')
+    return C / np.sqrt(np.outer(np.diagonal(C), np.diagonal(C)))
