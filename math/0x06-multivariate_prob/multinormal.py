08c8d9fcf216401837b0c3322acd9aa2e99a51f3
PauloMorillo <pauloan@hotmail.com>
2020-07-27 23:53:06 -0500

    [Create] Added directory and script 3

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -21,14 +21,12 @@ class MultiNormal():
 
     def pdf(self, x):
         """This method calculates the PDF at a data point"""
-        if type(x) is not np.ndarray:
-            raise TypeError('x must by a numpy.ndarray')
+        if x is None or type(x) is not np.ndarray:
+            raise TypeError('x must be a numpy.ndarray')
         d = self.cov.shape[0]
-        if len(x.shape) is not 2 or x.shape[0] is not d or \
-                x.shape[1] is not 1:
-            raise ValueError('x mush have the shape ({}, 1)'.format(d))
-        mean = self.mean
-        cov = self.cov
-        factor1 = 1 / np.sqrt(((2 * np.pi) ** d) * np.linalg.det(cov))
-        factor2 = np.exp(-(np.linalg.solve(cov, x - mean).T.dot(x - mean)) / 2)
-        return (factor1 * factor2)[0][0]
+        if len(x.shape) != 2 or x.shape[0] != d or x.shape[1] != 1:
+            raise ValueError('x must have the shape ({}, 1)'.format(d))
+        x_m = x - self.mean
+        result = (1. / (np.sqrt((2 * np.pi) ** d * np.linalg.det(self.cov))) *
+                  np.exp(-(np.linalg.solve(self.cov, x_m).T.dot(x_m)) / 2))
+        return result[0][0]
