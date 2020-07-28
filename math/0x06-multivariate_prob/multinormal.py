189502ba568f5ebf16abc45d6cf2a0c8564cd78a
PauloMorillo <pauloan@hotmail.com>
2020-07-27 22:34:54 -0500

    [Create] Added directory and script 2

---
+++ math/0x06-multivariate_prob/multinormal.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""This module has the class MultiNormal"""
+
+
+import numpy as np
+
+class MultiNormal():
+    """
+    This class represents a Multivariate Normal distribution
+    """
+    def __init__(self, data):
+        """All begins here"""
+        if type(data) is not np.ndarray or np.ndim(data) is not 2:
+            raise TypeError('X must be a 2D numpy.ndarray')
+        if data.shape[0] < 2:
+            raise ValueError('X must contain multiple data points')
+        self.mean = np.array([np.mean(data, axis=1)]).T
+        resta = data - self.mean
+        self.cov = np.dot(data, resta.T) / (data.shape[1] - 1)
