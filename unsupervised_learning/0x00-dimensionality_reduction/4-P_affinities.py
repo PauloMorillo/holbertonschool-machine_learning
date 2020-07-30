9639fb2512bbba91bac7a0ffcf8611bc018936f9
PauloMorillo <pauloan@hotmail.com>
2020-07-29 23:19:58 -0500

    [create] added perrada

--- unsupervised_learning/0x00-dimensionality_reduction/4-P_affinities.py
+++ unsupervised_learning/0x00-dimensionality_reduction/4-P_affinities.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python3
-""" This module has the method Q_affinities(Y)"""
+""" This module has the method P_affinities(X, tol=1e-5, perplexity=30.0)"""
 
 import numpy as np
 
+P_init = __import__('2-P_init').P_init
+HP = __import__('3-entropy').HP
 
-def Q_affinities(Y):
+
+def P_affinities(X, tol=1e-5, perplexity=30.0):
     """
-    Y is a numpy.ndarray of shape (n, ndim) containing the low
-    dimensional transformation of X n is the number of points
-    ndim is the new dimensional representation of X
-    Returns: Q, num Q is a numpy.ndarray of shape (n, n)
-    containing the Q affinities num is a numpy.ndarray of
-    shape (n, n) containing the numerator of the Q affinities
+    X is a numpy.ndarray of shape (n, d) containing the dataset to
+    be transformed by t-SNE
+    n is the number of data points
+    d is the number of dimensions in each point
+    perplexity is the perplexity that all Gaussian distributions
+    should have
+    tol is the maximum tolerance allowed (inclusive) for the difference
+    in Shannon entropy from perplexity for all Gaussian distributions
+    You should use P_init = __import__('2-P_init').P_init and
+    HP = __import__('3-entropy').HP
+    Returns: P, a numpy.ndarray of shape (n, n) containing the symmetric
+    P affinities
     """
     sum_Y = np.sum(np.square(Y), 1)
     Di = np.add(np.add(-2 * np.dot(Y, Y.T), sum_Y).T, sum_Y)
@@ -19,4 +28,4 @@ def Q_affinities(Y):
     num = 1 / (1 + Di)
     np.fill_diagonal(num, 0)
     Qi = num / np.sum(num)
-    return Qi, num
+    return Qi
