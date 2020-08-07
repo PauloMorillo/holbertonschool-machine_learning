b1eb11e03431b0a15f581f9ee3ba4198d5e93c3d
PauloMorillo <pauloan@hotmail.com>
2020-08-07 13:55:02 -0500

    [update] Fixed checkers tasks 7

--- unsupervised_learning/0x01-clustering/7-maximization.py
+++ unsupervised_learning/0x01-clustering/7-maximization.py
@@ -20,15 +20,19 @@ def maximization(X, g):
     S is a numpy.ndarray of shape (k, d, d) containing the updated
     covariance matrices for each cluster
     """
-    K = g.shape[0]
-    d = X.shape[1]
-    m = np.zeros((K, d))
-    S = np.zeros((K, d, d))
-    pi = np.sum(g / len(g[0]), axis=1)
-    # print(pi)
-    for k in range(K):
-        m[k] = np.matmul(g[k], X) / np.sum(g[k])
-        x_m = (X - m[k])
-        # print(x_m.shape)
-        S[k] = np.matmul(g[k] * (X - m[k]).T, x_m) / np.sum(g[k])
-    return pi, m, S
+    try:
+        K = g.shape[0]
+        if int(np.sum(g)) != g.shape[1]:
+            return None, None, None
+        d = X.shape[1]
+        m = np.zeros((K, d))
+        S = np.zeros((K, d, d))
+        pi = np.sum(g / len(g[0]), axis=1)
+        for k in range(K):
+            m[k] = np.matmul(g[k], X) / np.sum(g[k])
+            x_m = (X - m[k])
+            # print(x_m.shape)
+            S[k] = np.matmul(g[k] * (X - m[k]).T, x_m) / np.sum(g[k])
+        return pi, m, S
+    except Exception as e:
+        return None, None, None
