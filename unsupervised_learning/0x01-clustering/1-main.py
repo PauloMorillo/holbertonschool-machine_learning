55abf02ef29a4350eb3b8a5cb8e6044473d7d9ab
PauloMorillo <pauloan@hotmail.com>
2020-08-07 09:48:42 -0500

    [update] Fixed checkers

--- unsupervised_learning/0x01-clustering/1-main.py
+++ unsupervised_learning/0x01-clustering/1-main.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
 import numpy as np
-import matplotlib.pyplot as plt
 kmeans = __import__('1-kmeans').kmeans
 
 if __name__ == "__main__":
-    np.random.seed(0)
+    np.random.seed(1)
     a = np.random.multivariate_normal([30, 40], [[16, 0], [0, 16]], size=50)
     b = np.random.multivariate_normal([10, 25], [[16, 0], [0, 16]], size=50)
     c = np.random.multivariate_normal([40, 20], [[16, 0], [0, 16]], size=50)
@@ -13,8 +12,6 @@ if __name__ == "__main__":
     e = np.random.multivariate_normal([20, 70], [[16, 0], [0, 16]], size=50)
     X = np.concatenate((a, b, c, d, e), axis=0)
     np.random.shuffle(X)
-    C, clss = kmeans(X, 5)
+    C, clss = kmeans(X, 5, iterations=1000000000)
     print(C)
-    plt.scatter(X[:, 0], X[:, 1], s=10, c=clss)
-    plt.scatter(C[:, 0], C[:, 1], s=50, marker='*', c=list(range(5)))
-    plt.show()
+    print(clss)
