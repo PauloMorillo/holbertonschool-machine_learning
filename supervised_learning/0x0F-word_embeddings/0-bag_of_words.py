ddffc09becaa34f0a5d9a39a4c1d6ccb26d4001f
PauloMorillo <pauloan@hotmail.com>
2020-09-15 15:32:29 -0500

    [update] change logic with scikitlearn

--- supervised_learning/0x0F-word_embeddings/0-bag_of_words.py
+++ supervised_learning/0x0F-word_embeddings/0-bag_of_words.py
@@ -1,25 +1,22 @@
 #!/usr/bin/env python3
-
-from gensim import utils, corpora
-import numpy as np
+"""This module has the function bag_of_words"""
+from sklearn.feature_extraction.text import CountVectorizer
 
 
 def bag_of_words(sentences, vocab=None):
-    """This function creates a bag of words"""
-    features = set()
-    doc_tokenized = [utils.simple_preprocess(doc) for doc in sentences]
-    for sentence in sentences:
-        words = utils.simple_preprocess(sentence)
-        [features.add(word) for word in words]
-    # print(sorted(features), len(features))
-    embedded = np.zeros((len(sentences), len(features)))
-    dic_features = {}
-    for i, feaute in enumerate(sorted(features)):
-        # print(i, feaute)
-        dic_features[feaute] = i
-
-    for i, sentence in enumerate(doc_tokenized):
-        for word in sentence:
-            embedded[i, dic_features[word]] = int(
-                embedded[i, dic_features[word]] + 1)
-    return embedded, sorted(features)
+    """
+    This function creates a bag of words
+    sentences is a list of sentences to analyze
+    vocab is a list of the vocabulary words to use for the analysis
+    If None, all words within sentences should be used
+    Returns: embeddings, features
+    embeddings is a numpy.ndarray of shape (s, f) containing the embeddings
+    s is the number of sentences in sentences
+    f is the number of features analyzed
+    features is a list of the features used for embeddings
+    """
+    vectorizer = CountVectorizer(vocabulary=vocab)
+    X = vectorizer.fit_transform(sentences)
+    embedded = vectorizer.get_feature_names()
+    features = X.toarray()
+    return embedded, features
