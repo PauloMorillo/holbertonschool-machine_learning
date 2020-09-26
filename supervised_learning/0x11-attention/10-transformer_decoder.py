2daaa2dc9de5349398220ef8625f886e58cfe739
PauloMorillo <pauloan@hotmail.com>
2020-09-25 19:19:01 -0500

    [update] Added script forr task 10

---
+++ supervised_learning/0x11-attention/10-transformer_decoder.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+This module has the class Decoder
+"""
+
+import tensorflow as tf
+
+positional_encoding = __import__('4-positional_encoding').positional_encoding
+DecoderBlock = __import__('8-transformer_decoder_block').DecoderBlock
+
+
+class Decoder(tf.keras.layers.Layer):
+    """
+    This class perfoms Decoder
+    """
+
+    def __init__(self, N, dm, h, hidden, target_vocab,
+                 max_seq_len, drop_rate=0.1):
+        """
+        All begins here
+        """
+        super(Decoder, self).__init__()
+
+        self.N = N
+        self.dm = dm
+
+        self.embedding = tf.keras.layers.Embedding(target_vocab, dm)
+        self.positional_encoding = positional_encoding(max_seq_len, dm)
+
+        self.blocks = [DecoderBlock(dm, h, hidden, drop_rate)
+                       for _ in range(N)]
+        self.dropout = tf.keras.layers.Dropout(drop_rate)
+
+    def call(self, x, encoder_output, training,
+             look_ahead_mask, padding_mask):
+        """
+        This method call the model
+        """
+        seq_len = x.shape[1]
+
+        x = self.embedding(x)  # (batch_size, target_seq_len, d_model)
+        x *= tf.math.sqrt(tf.cast(self.dm, tf.float32))
+        x += self.positional_encoding_encoding[:seq_len]
+
+        x = self.dropout(x, training=training)
+
+        for i in range(self.N):
+            x = self.blocks[i](x, encoder_output, training,
+                               look_ahead_mask, padding_mask)
+
+        # x.shape == (batch_size, target_seq_len, d_model)
+        return x
