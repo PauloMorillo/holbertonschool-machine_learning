5e5885d90d27b60a4e5ef3ce869d3e232b0e6682
PauloMorillo <pauloan@hotmail.com>
2020-10-06 14:31:37 -0500

    [update] trying to solve issues

--- supervised_learning/0x12-transformer_apps/0-dataset.py
+++ supervised_learning/0x12-transformer_apps/0-dataset.py
@@ -10,14 +10,16 @@ class Dataset():
 
     def __init__(self):
         """ All begins here """
-        self.data_train,
-        self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en',
-                                    split=['train', 'validation'],
-                                    as_supervised=True)
-        self.tokenizer_pt,
-        self.tokenizer_en = self.tokenize_dataset(self.data_train)
-        # self.data_train = self.data_train.map(self.tf_encode)
-        # self.data_valid = self.data_valid.map(self.tf_encode)
+        self.data_train, self.data_valid = tfds.load(
+            'ted_hrlr_translate/pt_to_en',
+            split=['train', 'validation'],
+            as_supervised=True
+        )
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
+            self.data_train
+        )
+        self.data_train = self.data_train.map(self.tf_encode)
+        self.data_valid = self.data_valid.map(self.tf_encode)
 
     def tokenize_dataset(self, data):
         """
