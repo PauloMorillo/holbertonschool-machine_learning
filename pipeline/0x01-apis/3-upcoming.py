590ba7aff23e5582df712860b0e96201270088a3
PauloMorillo <pauloan@hotmail.com>
2020-10-28 12:41:50 -0500

    [update] added code for script 3

--- pipeline/0x01-apis/3-upcoming.py
+++ pipeline/0x01-apis/3-upcoming.py
@@ -14,18 +14,26 @@ def get_data(url):
     """
     header = {"Accept": "application/vnd.github.v3+json"}
     data = requests.get(url, params=header)
+    data = data.json()
     return data
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        data = get_data(sys.argv[1])
-        if data.status_code == 404:
-            print("Not found")
-        elif data.status_code == 403:
-            print("Reset in {} min".format(
-                (int(data.headers["X-Ratelimit-Reset"]) - time.time()) / 60
-            ))
-        else:
-            data = data.json()
-            print(data["location"])
+    all_data = get_data("https://api.spacexdata.com/v4/launches/upcoming")
+    time_c = all_data[0]["date_unix"]
+    pos = 0
+    for i, data in enumerate(all_data):
+        if time_c > data["date_unix"]:
+            time_c = data["date_unix"]
+            pos = i
+    url = "https://api.spacexdata.com/v4/launchpads/" + \
+          all_data[pos]["launchpad"]
+    data_lp = get_data(url)
+    url2 = "https://api.spacexdata.com/v4/rockets/" + all_data[pos]["rocket"]
+    data_r = get_data(url)
+    print("{} ({}) {} - {} ({})".format(all_data[pos]["name"],
+                                        all_data[pos]["date_local"],
+                                        data_r["name"],
+                                        data_lp["name"],
+                                        data_lp["locality"]
+                                        ))
