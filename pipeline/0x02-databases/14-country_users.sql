69ac5e1cb9eaa9b698b2f18d7bc2d1965f21f75e
root <pauloan@hotmail.com>
2020-11-07 23:50:05 +0000

    [update] added script for task 14

---
+++ pipeline/0x02-databases/14-country_users.sql
@@ -0,0 +1,8 @@
+-- This script creates  table called users in the current database
+CREATE TABLE IF NOT EXISTS users (
+id integer NOT NULL AUTO_INCREMENT,
+email VARCHAR(255) NOT NULL UNIQUE,
+name VARCHAR(255),
+country ENUM('US', 'CO', 'TN') NOT NULL,
+PRIMARY KEY (id)
+);
