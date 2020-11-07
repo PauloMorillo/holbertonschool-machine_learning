a4cf04a8270feb10296c60c7ce3b39168b6f75e4
root <pauloan@hotmail.com>
2020-11-07 23:06:00 +0000

    [update] instructions with uppercase

--- pipeline/0x02-databases/11-rating_shows.sql
+++ pipeline/0x02-databases/11-rating_shows.sql
@@ -1,2 +1,2 @@
 -- This script lists all shows from hbtn_0d_tvshows_rate by their rating
-select tv_shows.title, SUM(tv_show_ratings.rate) as rating from tv_shows INNER JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id GROUP by tv_shows.title ORDER by rating DESC;
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating FROM tv_shows INNER JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id GROUP by tv_shows.title ORDER by rating DESC;
