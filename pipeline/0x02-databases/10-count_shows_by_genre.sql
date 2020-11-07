ac164857d978647d0d79d64e723a798c87c256e8
root <pauloan@hotmail.com>
2020-11-07 03:38:25 +0000

    [update] added scripts until 10 task

---
+++ pipeline/0x02-databases/10-count_shows_by_genre.sql
@@ -0,0 +1,3 @@
+-- Script that select all cities in the database
+-- select cities from states
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id GROUP BY tv_show_genres.genre_id ORDER BY number_of_shows DESC;
