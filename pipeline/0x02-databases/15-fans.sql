dfca6762423ca7407355bc18aa4080a114ad3b99
root <pauloan@hotmail.com>
2020-11-08 00:05:04 +0000

    [update] added script for task 15

---
+++ pipeline/0x02-databases/15-fans.sql
@@ -0,0 +1,2 @@
+-- This script ranks country origins of bands, ordered by the number of (non-unique) fans
+SELECT origin, SUM(fans) AS nb_fans FROM `metal_bands` GROUP BY origin ORDER BY nb_fans DESC;
