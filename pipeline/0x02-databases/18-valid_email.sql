28b5e0cdceede7e5984091659d02419c6dd0cd17
root <pauloan@hotmail.com>
2020-11-08 14:31:10 +0000

    [update] taking account Jorge's advice

--- pipeline/0x02-databases/18-valid_email.sql
+++ pipeline/0x02-databases/18-valid_email.sql
@@ -1,2 +1,12 @@
 -- This script creates a trigger that resets the attribute valid_email only when the email has been changed
-CREATE TRIGGER `user_uu` BEFORE UPDATE ON `users` FOR EACH ROW IF OLD.email <> NEW.email THEN SET NEW.valid_email = 0; END IF;
+DELIMITER $$
+CREATE TRIGGER reset_valid
+    BEFORE UPDATE
+        ON users FOR EACH ROW
+	BEGIN
+	    IF OLD.email <> NEW.email THEN
+	           SET NEW.valid_email = 0;
+		       END IF;
+		       END $$
+
+DELIMITER ;
