2dd96f631f2ee68f2beacea9ba864457f551ce54
root <pauloan@hotmail.com>
2020-11-09 01:54:04 +0000

    [update] change name of input

--- pipeline/0x02-databases/20-average_score.sql
+++ pipeline/0x02-databases/20-average_score.sql
@@ -1,9 +1,9 @@
 -- This script creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student.
 DELIMITER $$
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN u_id INT)
 
 BEGIN
-UPDATE users SET average_score = (SELECT AVG(score) FROM corrections WHERE user_id = user_id) WHERE id = user_id;
+UPDATE users SET average_score = (SELECT AVG(score) FROM corrections WHERE user_id = u_id) WHERE id = u_id;
 END $$
 
 DELIMITER ;
