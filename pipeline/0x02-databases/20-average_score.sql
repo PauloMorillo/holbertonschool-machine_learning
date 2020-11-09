5e95621239001f62bc6a73e4cc955a9bd5faf781
root <pauloan@hotmail.com>
2020-11-09 01:51:56 +0000

    [update] remove unnecessary group

--- pipeline/0x02-databases/20-average_score.sql
+++ pipeline/0x02-databases/20-average_score.sql
@@ -3,7 +3,7 @@ DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 
 BEGIN
-UPDATE users SET average_score = (SELECT AVG(score) FROM corrections WHERE user_id = user_id GROUP BY user_id) WHERE id = user_id;
+UPDATE users SET average_score = (SELECT AVG(score) FROM corrections WHERE user_id = user_id) WHERE id = user_id;
 END $$
 
 DELIMITER ;
